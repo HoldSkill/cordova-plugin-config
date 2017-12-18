c868fe87927f298161b5f1b021fae27edc5816e0
Charles <lcq@holdskill.com>
2017-12-18 21:59:09 +0800

    014
    014

--- www/CordovaPluginConfig.js
+++ www/CordovaPluginConfig.js
@@ -2,17 +2,17 @@ var exec = require('cordova/exec');
 module.exports = {
     LongPressFix: function(callback) {
         console.log('CordovaPluginConfig.LongPressFix');
-        exec(callback, this._AudioPermissionErrorEvent, 'CordovaPluginConfig', 'LongPressFix', []);
+        exec(callback, this.AudioPermissionErrorEvent, 'CordovaPluginConfig', 'LongPressFix', []);
     },
     checkAudioPermission: function(callback) {
         console.log('CordovaPluginConfig.checkAudioPermission');
-        exec(callback, this._AudioPermissionErrorEvent, 'CordovaPluginConfig', 'checkAudioPermission', []);
+        exec(callback, this.AudioPermissionErrorEvent, 'CordovaPluginConfig', 'checkAudioPermission', []);
     },
     getAudioPermission: function(callback) {
         console.log('CordovaPluginConfig.getAudioPermission');
-        exec(callback, this._AudioPermissionErrorEvent, 'CordovaPluginConfig', 'getAudioPermission', []);
+        exec(callback, this.AudioPermissionErrorEvent, 'CordovaPluginConfig', 'getAudioPermission', []);
     },
-    _AudioPermissionErrorEvent = function(e) {
+    AudioPermissionErrorEvent: function(e) {
         cordova.fireWindowEvent("AudioPermissionRrror", {
             message: e
         });
