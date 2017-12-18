9d5d75b85320ff3e3f443c4cc92741708fc1367b
Charles <lcq@holdskill.com>
2017-12-18 17:39:00 +0800

    012
    012

--- www/CordovaPluginConfig.js
+++ www/CordovaPluginConfig.js
@@ -3,15 +3,15 @@ var CordovaPluginConfig = {};
 CordovaPluginConfig.LongPressFix = function(callback) {
     console.log('CordovaPluginConfig.LongPressFix');
     exec(callback, CordovaPluginConfig._AudioPermissionErrorEvent, 'CordovaPluginConfig', 'LongPressFix', []);
-}
+};
 CordovaPluginConfig.checkAudioPermission = function(callback) {
     console.log('CordovaPluginConfig.checkAudioPermission');
     exec(callback, CordovaPluginConfig._AudioPermissionErrorEvent, 'CordovaPluginConfig', 'checkAudioPermission', []);
-}
+};
 CordovaPluginConfig.getAudioPermission = function(callback) {
     console.log('CordovaPluginConfig.getAudioPermission');
     exec(callback, CordovaPluginConfig._AudioPermissionErrorEvent, 'CordovaPluginConfig', 'getAudioPermission', []);
-}
+};
 CordovaPluginConfig._AudioPermissionErrorEvent = function(e) {
     cordova.fireWindowEvent("AudioPermissionRrror", {
         message: e
